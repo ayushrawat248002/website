9d15712f6279c9989392129fd24130631f53b1e3
Ayush <Ayush248002@gmail.com>
2026-04-14 21:43:40 +0530

    initial commit

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,16 +1,16 @@
 
 
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
-import { Switch } from "radix-ui"
+import * as SwitchPrimitive from "@radix-ui/react-switch"
 export function SwitchSizes() {
   return (
     <FieldGroup className="w-full max-w-[10rem]">
       <Field orientation="horizontal">
-        <Switch id="switch-size-sm" size="sm" />
+        <SwitchPrimitive.Root id="switch-size-sm" />
         <FieldLabel htmlFor="switch-size-sm">Small</FieldLabel>
       </Field>
       <Field orientation="horizontal">
-        <Switch id="switch-size-default" size="default" />
+        <SwitchPrimitive.Root id="switch-size-default" />
         <FieldLabel htmlFor="switch-size-default">Default</FieldLabel>
       </Field>
     </FieldGroup>
